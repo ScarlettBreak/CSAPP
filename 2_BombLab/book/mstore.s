47c5a4d1182728506ebef938ec007956301f20f5
ScarlettBreak <498547018@qq.com>
2019-01-30 15:35:01 +0800

    书上的代码
    帮助理解

---
+++ 2_BombLab/book/mstore.s
@@ -0,0 +1,52 @@
+	.file	"mstore.c"
+	.text
+	.globl	multstore
+	.type	multstore, @function
+multstore:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	subl	$8, %esp
+	pushl	12(%ebp)
+	pushl	8(%ebp)
+	call	mult2
+	addl	$16, %esp
+	movl	%eax, -12(%ebp)
+	movl	16(%ebp), %eax
+	movl	-12(%ebp), %edx
+	movl	%edx, (%eax)
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	multstore, .-multstore
+	.globl	mytest
+	.type	mytest, @function
+mytest:
+.LFB1:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	movl	$0, -4(%ebp)
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	mytest, .-mytest
+	.ident	"GCC: (Ubuntu 5.5.0-12ubuntu1~16.04) 5.5.0 20171010"
+	.section	.note.GNU-stack,"",@progbits
